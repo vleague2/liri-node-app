9e7b971dd5d437d0144cc279f5c446657ac4426d
unknown <vleague2@gmail.com>
2018-04-19 19:09:40 -0400

    set up twitter function

--- liri.js
+++ liri.js
@@ -9,6 +9,8 @@ const Twitter = require("twitter");
 
 const request = require("request");
 
+const chalk = require('chalk');
+
 // Setting up variables
 
 const spotify = new Spotify(keys.spotify);
@@ -18,10 +20,6 @@ const twitter = new Twitter(keys.twitter);
 const command = process.argv[2];
 
 const input = process.argv[3];
-
-console.log(keys);
-
-console.log(spotify);
 
 // Switch statement for the app to decide what to do
 
@@ -43,9 +41,22 @@ switch (command) {
     break;   
 }
 
-// show your last 20 tweets and when they were created at in your terminal/bash window.
+// shows your last 20 tweets and when they were created at in your terminal/bash window.
 function myTweets() {
+    let params = {screen_name: '_callmelight'};
+    
+    twitter.get('statuses/user_timeline', params,(error, tweets, response) => {
+        if (!error) {
+        
+            for (i=0; i < 20; i++) {
+                let number = parseInt(i) + 1;
+                let date = tweets[i].created_at;
+                let tweet = tweets[i].text;
 
+                console.log(chalk.red("Tweet " + number + " created on " + date + ": ") + tweet);
+            }
+        }
+    });
 };
 
 // show the following information about the song in your terminal/bash window: 1) Artist(s), 2) the song's name, 3) a preview link of the song from spotify, 4) the album, 5) if no song is provided then it will default to 'the sign' by ace of base
@@ -55,14 +66,16 @@ function spotifySong() {
 
 // output the following information to your terminal/bash window: Title of the movie, Year the movie came out, IMDB Rating of the movie, Rotten Tomatoes Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie. If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
 function movieThis() {
-    request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", (error, response, body) => {
 
+        if (input == undefined) {
+            console.log("Mr. Nobody");
+        }
         
-        if (!error && response.statusCode === 200) {
+        else if (!error && response.statusCode === 200) {
 
             // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            console.log(chalk.red("The movie's rating is: ") + JSON.parse(body).imdbRating);
         }
     });
 };
